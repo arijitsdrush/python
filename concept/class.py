5a4c91e58204449956fb897731ffde9e06265604
arijit <riju.srk@gmail.com>
2017-10-26 17:06:40 +0530

    Updating code for fibbonaci

--- concept/class.py
+++ concept/class.py
@@ -1,6 +1,15 @@
-class fibonacci():
+class math_operation :
     
-    def __init__(self, n):
-        self.n = n
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
         
+    def add (self):
         
+        return self.a + self.b        
+    
+
+
+obj = math_operation (1,2)      
+
+print(dir(obj))
---
+++ concept/fibbonaci.py
@@ -0,0 +1,7 @@
+a , b =0 ,1
+
+for i in range(1,10):
+    
+        a , b = b, a + b
+        print(b)
+            
